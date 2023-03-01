4645824b122873f464d140eba393dbafd6a140b9
Gopichand <nallanigopichand@gmail.com>
2023-03-01 11:10:06 +0000

    uodateing the databse based on the number of values provided

--- service/routers.js
+++ service/routers.js
@@ -46,14 +46,46 @@ router.put("/:id", (req, res) =>{
     Description: req.body.Description,
     State: req.body.State
   };
-  if (payload.Task != null) {
-  db.query(`UPDATE todo set Task = "${payload.Task}" WHERE id = "${id}"`, (err, rows) => {
-    if (!err) {
-      res.json(rows);
-    }
-    else
-    console.log(err)
-  })
+  if (payload.Description != null && payload.Task != null && payload.State != null ) {
+    db.query(`UPDATE todo set State = "${payload.State}", Description = "${payload.Description}", Task = "${payload.Task}" WHERE id = "${id}"`, (err, rows) => {
+      if (!err) {
+        res.json(rows);
+      }
+      else
+      console.log(err)
+    })
+  } else if (payload.State != null && payload.Description != null) {
+    db.query(`UPDATE todo set State = "${payload.State}", Description = "${payload.Description}" WHERE id = "${id}"`, (err, rows) => {
+      if (!err) {
+        res.json(rows);
+      }
+      else
+      console.log(err)
+    })
+  } else if (payload.State != null && payload.Task != null) {
+    db.query(`UPDATE todo set State = "${payload.State}", Task = "${payload.Task}"  WHERE id = "${id}"`, (err, rows) => {
+      if (!err) {
+        res.json(rows);
+      }
+      else
+      console.log(err)
+    })
+  } else if (payload.Description != null && payload.Task != null) {
+    db.query(`UPDATE todo set Description = "${payload.Description}", Task = "${payload.Task}" WHERE id = "${id}"`, (err, rows) => {
+      if (!err) {
+        res.json(rows);
+      }
+      else
+      console.log(err)
+    })
+  } else if (payload.Task != null) {
+    db.query(`UPDATE todo set Task = "${payload.Task}" WHERE id = "${id}"`, (err, rows) => {
+      if (!err) {
+        res.json(rows);
+      }
+      else
+      console.log(err)
+    })
   } else if (payload.Description != null) {
     db.query(`UPDATE todo set Description = "${payload.Description}" WHERE id = "${id}"`, (err, rows) => {
       if (!err) {
@@ -70,7 +102,7 @@ router.put("/:id", (req, res) =>{
       else
       console.log(err)
     })
-  }
+  } 
 })
 
 module.exports = router;
